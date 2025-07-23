e88ac6d537f8e7845da1d7ab94fdb7981ad66010
Yueling Liu <stellaliuca@gmail.com>
2025-07-23 12:32:45 -0700

    delete

--- routes/book.js
+++ routes/book.js
@@ -5,6 +5,7 @@ const booksController = require('../controllers/book');
 // http request
 router.get('/', booksController.getAllBooks);
 
+// to delete a book by id
 router.delete('/:id', booksController.deleteBook);
 
 module.exports = router;
